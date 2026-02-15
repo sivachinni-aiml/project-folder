e0ce9e46f0bd432978bd3ac99b8eb64c5fc30e23
sivachinni-aiml <2025aa05793@wilp.bits-pilani.ac.in>
2026-02-15 23:46:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,9 +85,7 @@ with st.sidebar:
 
 # Selected model
 info = artifacts[model_name]
-#model = load_model(info['model_file'])
-model = load_model(XGBoost_Ensemble.joblib)
-XGBoost_Ensemble.joblib
+model = load_model(info['model_file'])
 
 col1, col2 = st.columns([1.25, 1])
 
